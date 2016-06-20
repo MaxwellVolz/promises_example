eb30788ccf2a2ef98683c69446218c1275e0c7e0
Max Volz <mvolz@advisor.asi>
2016-06-20 11:40:07 -0700

    converted DOM appending function to Vanilla javascript; added error message handling

--- promises.js
+++ promises.js
@@ -31,15 +31,39 @@ function get(url) {
 
 get('data.json').then(function(response) {
 
-    var json = $.parseJSON(response);
-
-    $.each(json, function(index, value) {
-        console.log("Name is: " + value.name);
-
-        $('body').append('<h2>' + value.name + '</h2>');
-    });
-
     console.log("Success!", response);
+
+    var json = $.parseJSON(response),
+        content = document.getElementById("content");
+
+    // ------------------------------------
+    // Javascript version of DOM Appending
+    // ------------------------------------
+    json.map(function(obj){
+        var node = document.createElement("LI");
+        var textnode = document.createTextNode(obj.name); 
+        node.appendChild(textnode);
+
+        content.appendChild(node);
+    })
+
+    // ------------------------------------
+    // jQuery version of DOM Appending
+    // ------------------------------------
+    // $.each(json, function(index, value) {
+    //     console.log("Name is: " + value.name);
+
+    //     $('body').append('<h2>' + value.name + '</h2>');
+    // });
+
+    
 }, function(error) {
     console.error("Failed!", error);
+
+    var node = document.createElement("LI"),
+        textnode = document.createTextNode(error); 
+
+    node.appendChild(textnode);
+    content.appendChild(node);
+
 });
